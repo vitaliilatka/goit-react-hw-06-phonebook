ebda605b11e0e243433f8274c1db4404bd8ce750
vetallatka <latka.vitalik@yandex.ru>
2021-04-24 21:21:59 +0200

    made store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,37 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import contacts from '../redux/contacts/contactsReducer';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from '../redux/contacts/contactsReducer';
 
-const rootReducer = combineReducers({
-    contacts,
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+];
+
+const contactsPersistConfig = {
+    key: 'contacts',
+    storage,
+    blacklist: ['filter'],
+};
+
+const store = configureStore({
+    reducer: { contacts: persistReducer(contactsPersistConfig, contactsReducer) },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistor = persistStore(store);
 
-export default store;
+export default (store, persistor);
